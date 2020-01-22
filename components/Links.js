42a0d3e40c35e0801964835369c7469cbbcda827
Evan Bacon <baconbrix@gmail.com>
2020-01-22 15:12:26 -0800

    Create Links.js

---
+++ components/Links.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Linking, Platform, StyleSheet, Text, View } from 'react-native';
+
+export default function Links({ show }) {
+  return (
+    <View style={[styles.linkContainer, { opacity: show ? 0.6 : 0.0 }]} pointerEvents={show ? 'auto' : 'none'}>
+      <Text style={styles.link} accessibilityRole="link" href={link} target="_blank" onPress={() => Linking.openURL(link)}>Evan Bacon 🥓</Text>
+      <Text style={styles.link} accessibilityRole="link" href={sourceLink} target="_blank" onPress={() => Linking.openURL(sourceLink)}>{`</>`}</Text>
+    </View>
+  )
+}
+
+const link = 'https://twitter.com/baconbrix'
+const sourceLink = 'https://github.com/EvanBacon/pixi-snake'
+
+const styles = StyleSheet.create({
+  linkContainer: {
+    ...Platform.select({ web: { transitionDuration: '0.2s' }, default: {} }),
+    position: 'absolute',
+    top: 24,
+    left: 24,
+  },
+  link: {
+    fontFamily: 'kombat',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
