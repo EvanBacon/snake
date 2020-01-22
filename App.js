c52cb7e6286cba431f92e53b2d820699d27a45a7
Evan Bacon <baconbrix@gmail.com>
2020-01-22 14:47:31 -0800

    Add custom font for score

--- App.js
+++ App.js
@@ -1,64 +1,106 @@
-import { GLView } from 'expo-gl';
+import { AppLoading, SplashScreen, Updates } from 'expo';
+import { Asset } from 'expo-asset';
 import Constants from 'expo-constants';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Animated, Button, StyleSheet, Text, View } from 'react-native';
+import GameScreen from './GameScreen';
+import * as Font from 'expo-font';
 
-import Game from './Game';
-import GestureView from './components/GestureView';
-import Colors from './constants/Colors';
+SplashScreen.preventAutoHide(); // Instruct SplashScreen not to hide yet
 
-export default class App extends React.Component {
-  state = { score: 0 };
-
-  onTap = () => {
-    if (this.game) {
-      this.game.board.onTap();
-    }
-  };
-
-  onSwipe = direction => {
-    if (this.game) {
-      this.game.board.onSwipe(direction);
-    }
-  };
-
-  onContextCreate = context => {
-    this.game = new Game(context);
-    this.game.board.onScore = score => this.setState({ score });
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <GestureView
-          style={{ flex: 1 }}
-          onTap={this.onTap}
-          onSwipe={this.onSwipe}
-        >
-          <GLView
-            style={{ flex: 1, height: '100%', overflow: 'hidden' }}
-            onContextCreate={this.onContextCreate}
-          />
-        </GestureView>
-        <Text style={styles.score}>{this.state.score}</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <AnimatedAppLoader image={require('./assets/loading.png')} />
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: Colors.primary,
-  },
-  score: {
-    fontSize: 48,
-    textAlign: 'right',
-    position: 'absolute',
-    top: 24,
-    right: 24,
-    opacity: 0.6,
-    fontWeight: 'bold',
-  },
-});
+function AnimatedAppLoader({ image }) {
+  const [isSplashReady, setSplashReady] = React.useState(false);
+
+  const startAsync = React.useMemo(
+    () => () => {
+      return Asset.fromModule(image).downloadAsync();
+    },
+    [image]
+  );
+
+  const onFinish = React.useMemo(() => setSplashReady(true), []);
+
+  if (!isSplashReady) {
+    return (
+      <AppLoading
+        startAsync={startAsync}
+        onError={console.error}
+        onFinish={onFinish}
+      />
+    );
+  }
+
+  return <AnimatedSplashScreen image={image} />
+}
+
+function AnimatedSplashScreen({ image }) {
+  const animation = React.useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = React.useState(false);
+  const [isGameReady, setGameReady] = React.useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = React.useState(
+    false
+  );
+
+  React.useEffect(() => {
+    if (isGameReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isGameReady]);
+
+  const onImageLoaded = React.useMemo(() => async () => {
+    SplashScreen.hide();
+    try {
+      // Load stuff
+      await Promise.all([
+        Font.loadAsync('kombat', require('./assets/kombat.ttf'))
+      ]);
+    } catch (e) {
+      // handle errors
+    } finally {
+        setAppReady(true);
+    }
+  });
+
+  return (
+    <View style={{ flex: 1 }}>
+        {isAppReady && <GameScreen onReady={() => setGameReady(true)} />}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}>
+          <Animated.Image
+            style={{
+              width: '100%',
+              height: '100%',
+              resizeMode: Constants.manifest.splash.resizeMode || 'contain',
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
---
+++ GameScreen.js
@@ -0,0 +1,66 @@
+import { GLView } from 'expo-gl';
+import Constants from 'expo-constants';
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Game from './Game';
+import GestureView from './components/GestureView';
+import Colors from './constants/Colors';
+
+export default class App extends React.Component {
+  state = { score: 0 };
+
+  onTap = () => {
+    if (this.game) {
+      this.game.board.onTap();
+    }
+  };
+
+  onSwipe = direction => {
+    if (this.game) {
+      this.game.board.onSwipe(direction);
+    }
+  };
+
+  onContextCreate = context => {
+    this.game = new Game(context);
+    this.game.board.onScore = score => this.setState({ score });
+    this.props.onReady()
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <GestureView
+          style={{ flex: 1 }}
+          onTap={this.onTap}
+          onSwipe={this.onSwipe}
+        >
+          <GLView
+            style={{ flex: 1, height: '100%', overflow: 'hidden' }}
+            onContextCreate={this.onContextCreate}
+          />
+        </GestureView>
+        <Text style={styles.score}>{this.state.score}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: Colors.primary,
+  },
+  score: {
+    fontFamily: 'kombat',
+    fontSize: 48,
+    textAlign: 'right',
+    position: 'absolute',
+    top: 24,
+    right: 24,
+    opacity: 0.6,
+    fontWeight: 'bold',
+  },
+});
