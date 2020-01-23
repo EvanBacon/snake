55d50842ee8feb7602532e364531f16f75833c4c
Evan Bacon <baconbrix@gmail.com>
2020-01-22 16:13:16 -0800

    update links

--- components/Links.js
+++ components/Links.js
@@ -4,8 +4,8 @@ import { Linking, Platform, StyleSheet, Text, View } from 'react-native';
 export default function Links({ show }) {
   return (
     <View style={[styles.linkContainer, { opacity: show ? 0.6 : 0.0 }]} pointerEvents={show ? 'auto' : 'none'}>
-      <Text style={styles.link} accessibilityRole="link" href={link} target="_blank" onPress={() => Linking.openURL(link)}>Evan Bacon 🥓</Text>
-      <Text style={styles.link} accessibilityRole="link" href={sourceLink} target="_blank" onPress={() => Linking.openURL(sourceLink)}>{`</>`}</Text>
+      <Text style={styles.link} accessibilityRole="link" href={link} target="_blank" onPress={() => Platform.OS !== 'web' && Linking.openURL(link)}>Evan Bacon 🥓</Text>
+      <Text style={styles.link} accessibilityRole="link" href={sourceLink} target="_blank" onPress={() => Platform.OS !== 'web' && Linking.openURL(sourceLink)}>{`</>`}</Text>
     </View>
   )
 }
